e0962d4ec4ce966cb0ded33423c74bdb0e2c1525
Johan Smarius <jsmarius@hotmail.com>
2018-01-26 10:06:35 +0100

    Added an extra test to show live unit testing

---
+++ DemoCourses/CourseTests/HelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CourseDomain;
+
+namespace CourseTests
+{
+    /// <summary>
+    /// Summary description for HelperTests
+    /// </summary>
+    [TestClass]
+    public class HelperTests
+    {
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var date = DateTime.Now;
+
+            //var result = TestClass.Format(date);
+
+            //Assert.AreEqual(date.ToLongDateString(), result);
+
+        }
+    }
+}
